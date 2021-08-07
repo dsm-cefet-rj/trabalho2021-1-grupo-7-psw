541a20cdaf8dbf18b5a084c016c1916bc4a12418
Lucas Farolfi <lucasfarolfi.rj@gmail.com>
2021-08-07 00:01:08 -0300

    Footer estático criado

---
+++ frontend/src/components/Footer/index.js
@@ -0,0 +1,32 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react'
+import { FooterComponent, Container, LinkList, Copyright, LogoContainer, Logo} from './style.js'
+import logoImg from '../../assets/img/logo.svg'
+
+export default function Footer(){
+    return(
+        <FooterComponent>
+            <Container>
+
+                <LogoContainer>
+                   <Logo src={logoImg}/> 
+                </LogoContainer>
+
+                <LinkList>
+                    <li>Home</li>
+                    <li>Seus Pedidos</li>
+                    <li>Suporte</li>
+                    <li>FeedBack</li>
+                    <li>Sobre</li>
+                    <li>Entrar</li>
+                    <li>Cadastrar-se</li>
+                </LinkList>
+
+                <hr/>
+
+                <Copyright>&#169; IngressosHub. Todos os direitos reservados</Copyright>    
+            </Container>
+            
+        </FooterComponent>
+    )
+}
