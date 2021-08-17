6b6dc3ca35ae52a7b5434d3786b793793a540486
João Marcos <joaomarcossleite@gmail.com>
2021-08-16 23:38:17 -0300

    Co-authored-by: Caio Abreu Ribeiro <Caio-Abreu@users.noreply.github.com>

--- frontend/src/components/Carousel/style.js
+++ frontend/src/components/Carousel/style.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 export const BannerEvent = styled.div`
   width: 100%;
-  height: 500px;
+  height: 600px;
   background: ${props => `url(${props.imgBanner}) no-repeat center`};
   background-size: cover;
 `;
---
+++ frontend/src/utils/events.js
@@ -0,0 +1,66 @@
+export const events = [
+  {
+    id: 1,
+    name: 'Um lugar silencionso 2',
+    type: 'Cinema',
+    descricao: 'lorem',
+    empresa: 'Paramount',
+    qtd_ingresso: 10,
+    imagens: ['https://tracklist.com.br/wp-content/uploads/2021/06/FOTO-54.jpg', 'https://www.hollywoodreporter.com/wp-content/uploads/2021/05/2cb5d5a5-3519-4b23-bf65-c4e31cbb714f-2.png']
+  },
+  {
+    id: 2,
+    name: 'Rock in Rio',
+    type: 'Show',
+    descricao: 'lorem',
+    empresa: 'Rock in Rio',
+    qtd_ingresso: 1,
+    imagens: ['https://tracklist.com.br/wp-content/uploads/2021/08/rock-in-rio.png']
+  },
+  {
+    id: 3,
+    name: 'Loki',
+    type: 'Cinema',
+    descricao: 'lorem',
+    empresa: 'Marvel',
+    qtd_ingresso: 13,
+    imagens: ['https://img.olhardigital.com.br/wp-content/uploads/2021/05/Loki-1.jpg']
+  },
+  {
+    id: 4,
+    name: 'CBLOL',
+    type: 'Evento',
+    descricao: 'lorem',
+    empresa: 'CBLOL',
+    qtd_ingresso: 2,
+    imagens: ['https://imagens.ebc.com.br/sy2XzPi9f9XV9zkKyZDZyBb_FD8=/1170x700/smart/https://agenciabrasil.ebc.com.br/sites/default/files/thumbnails/image/imagem_rio.jpg?itok=ikxPLMpd']
+  },
+  {
+    id: 5,
+    name: 'Velozes e Furiosos 9',
+    type: 'Filme',
+    descricao: 'lorem',
+    empresa: 'Universal',
+    qtd_ingresso: 20,
+    imagens: ['https://cdn.ome.lt/o4KVf1xxVkdCbqavTCQW-KWURJI=/1200x630/smart/extras/conteudos/Velozes_e_Furiosos_9_poster.jpg']
+  },
+  {
+    id: 6,
+    name: 'Major CSGO',
+    type: 'Evento',
+    descricao: 'lorem',
+    empresa: 'CSGO',
+    qtd_ingresso: 3,
+    imagens: ['https://s2.glbimg.com/RcC7UETKZDZVV_o84Bo4LsKLpD4=/0x0:2048x1365/1008x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2020/8/U/DBrBCdTtCYV7srdTUKZw/palco-major-berlin.jpg']
+  }, {
+    id: 7,
+    name: 'Teste',
+    type: 'Evento',
+    descricao: 'lorem',
+    empresa: 'CSGO',
+    qtd_ingresso: 3,
+    imagens: ['https://s2.glbimg.com/RcC7UETKZDZVV_o84Bo4LsKLpD4=/0x0:2048x1365/1008x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2020/8/U/DBrBCdTtCYV7srdTUKZw/palco-major-berlin.jpg']
+  }
+]
+
+export const getEventById = ((id) => events.find((event) => event.id === id))
