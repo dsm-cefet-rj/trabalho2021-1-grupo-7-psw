20e49cfdfa83ef2f244d30f2c77edc7835085a58
Caio Abreu <kbreugameplay@gmail.com>
2021-08-18 00:08:37 -0300

    Pagina sobre

---
+++ frontend/src/pages/About/style.js
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+
+export const Main = styled.main`
+    margin-bottom: 20px;
+    min-height: 85vh;
+`;
+
+export const Title = styled.h1`
+    font-weight: 550;
+    font-size: 22px;
+    width: 50%;
+    text-align: center;
+    color: white;
+    background-color: #212020;
+    border-radius: 60px;
+    word-break: break-word;
+    margin: 50px auto;
+    padding: 10px 0;
+`;
+
+export const Div = styled.div`
+    text-align:center;
+    margin: 20px;
+    height: 15em;
+    width: 15em;
+`;
+export const Img = styled.img`
+    border-radius: 50%;   
+    width: 100%;
+    height: 100%;
+    // margin: 20px;
+    border: 2px solid black;
+`;
+
+export const Container = styled.section`
+    padding: 1rem 10px;
+    margin: 0 auto;
+    max-width: 1100px;
+`;
+
+export const Text = styled.p`
+    margin: 10px 10%;
+    font-size: 16px;
+    font-weight: 550;
+`;
