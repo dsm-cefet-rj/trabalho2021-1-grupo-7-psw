ce601b763e9fbff846c85e4e3d1df74eab6dde82
lucasfarolfi <lucasfarolfi.rj@gmail.com>
2021-08-20 20:40:05 -0300

    Rotas de usuarios e eventos criadas

---
+++ backend/Controllers/EventController.js
@@ -0,0 +1,75 @@
+const Event = require('../Models/Event')
+const Slugify = require('slugify')
+
+class EventController{
+    async index(req, res){
+        let events = await Event.getAll()
+
+        res.status(200).json({events})
+    }
+
+    async create(req, res){
+        let {name, type, enterprise, num_tickets,date, price, description} = req.body
+
+        if((name == undefined || name == '') || (type == undefined || type == '') || (enterprise == undefined || enterprise == '')
+        || (num_tickets == undefined || num_tickets == '') || (price == undefined || price == '') || (date == undefined || date == '')
+        || (description == undefined || description == '')){
+            return res.status(400).json({error: "Dados inválidos"})
+        }
+
+        let slug = Slugify(name).toLowerCase()
+        let findEvent = await Event.findOne(slug)
+        
+        if(findEvent != undefined){
+            return res.status(401).json({error: "Evento já cadastrado"})
+        }
+
+        await Event.create(name, slug, type, enterprise, num_tickets,date, price, description)
+        return res.status(200).json({status: "Evento cadastrado com sucesso!"})
+    }
+
+    async find(req, res){
+        let slug = req.params.slug
+
+        let event = await Event.findOne(slug)
+
+        if(event == undefined){
+            return res.status(404).json({erro: "Evento não encontrado"})
+        }
+
+        return res.status(200).json({event})
+    }
+
+    async update(req, res){
+        let {slug ,name, type, num_tickets,date, price, description} = req.body
+        
+        let findEvent = await Event.findOne(slug)   
+
+        if(findEvent == undefined){
+            return res.status(404).json({error: "Evento não encontrado"})
+        }
+
+        await Event.update(name, slug, type, num_tickets,date, price, description)
+        return res.status(200).json({status: "Evento atualizado com sucesso!"})
+    }
+
+    async delete(req, res){
+        let slug = req.params.slug
+
+        if(slug == undefined || slug == ''){
+            return res.status(400).json({error: "Dados inválidos"})
+        }
+
+        let event = await Event.findOne(slug)
+
+        if(event == undefined){
+            return res.status(404).json({error: "Evento não encontrado"})
+        }
+
+        await Event.delete(slug)
+        return res.status(200).json({status: "Evento deletado com sucesso!"})
+    }
+
+}
+
+module.exports = new EventController()
