36a5dc988f5e16c1dcc8d21c7f6398ad46455e09
Felipe dos Santos <felipe-junior@users.noreply.github.com>
2021-08-20 17:54:05 -0300

    Inclusão de state

--- frontend/src/pages/Event/Create.jsx
+++ frontend/src/pages/Event/Create.jsx
@@ -1,81 +1,134 @@
 import {
-    PageTitle,
-    FormCardWrapper,
-    InputText,
-    Label,
-    Container,
-    FormTitle,InputTextarea
-  } from '../../components/Event/style';
+  PageTitle,
+  FormCardWrapper,
+  InputText,
+  Label,
+  Container,
+  FormTitle,InputTextarea, ErrorInputs
+} from './style';
 
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import SaveIcon from '@material-ui/icons/Save';
 import Header from '../../components/Header/index'
 import Footer from '../../components/Footer/index'
+import {useFormik} from 'formik'
+import * as yup from 'yup'
 
 const useStyles = makeStyles((theme) => ({
-    button: {
-        margin: theme.spacing(2),
-        width: 50 + "%",
-        "@media (max-width: 600px)": {
-            width: 100 + "%"
-          }
-    },
-    div:{
-        display: "flex",
-        justifyContent: "space-around"
-    },
-  }));
+  button: {
+      margin: theme.spacing(2),
+      width: 50 + "%",
+      "@media (max-width: 600px)": {
+          width: 100 + "%"
+        }
+  },
+  div:{
+      display: "flex",
+      justifyContent: "space-around"
+  },
+}));
 export default function FormCard(props) {
 
-    const classes = useStyles(); 
+const classes = useStyles(); 
 
-    return (
-      <>
-        <Header/>
-        <PageTitle>Criar evento</PageTitle>
-        <Container>
-          <FormCardWrapper>
-          <FormTitle style={{marginBottom: 25 + "px"} }>Novo evento</FormTitle>
+const formik = useFormik({
+  initialValues: {
+    name: '',
+    type: '',
+    quantity: '',
+    date: '',
+    price: '',
+    description: ''
+  },
+  validationSchema: yup.object({
+    name: yup
+            .string().required("O campo de nome precisa ser preenchido."),
+    type: yup
+            .string().required("O campo de tipo precisa ser preenchido."),
+    quantity: yup
+            .string().required("O campo de quantidade precisa ser preenchido."),
+    date: yup
+            .string().required("O campo de data precisa ser preenchido.")
+            .min(10, "O campo de data precisa ser preenchido corretamente."),
+    price: yup
+            .string().required("O campo de preço precisa ser preenchido."),
+    description: yup
+            .string().required("O campo de descrição precisa ser preenchido.")
+  }),
+    onSubmit: (values) =>{
+        //HandleSubmit
+        const event = {
+          name: formik.values.name,
+          type: formik.values.type,
+          quantity: formik.values.quantity,
+          date: formik.values.date,
+          price: formik.values.price,
+          description: formik.values.description
+        }
 
-          <Label htmlFor='eventName'>Nome do evento:</Label>
-          <InputText type='text' id='eventName'placeholder='Digite o nome do evento'/>
+        console.log(event)
+    }
+})
 
-          <Label htmlFor='eventType'>Tipo de evento: </Label>
-          <InputText type='text' id='eventType'placeholder='Digite o tipo do evento'/>
+  return (
+    <>
+      <Header/>
+      <PageTitle>Criar evento</PageTitle>
+      <Container>
+        <FormCardWrapper onSubmit={formik.handleSubmit}>
+        <FormTitle style={{marginBottom: 25 + "px"} }>Novo evento</FormTitle>
 
-          <Label htmlFor='eventTicketNumber'>Quantidade de ingressos:</Label>
-          <InputText type='number' id='eventTicketNumber'placeholder='Digite a quantidade de ingressos'/>
+        <Label htmlFor='eventName'>Nome do evento:</Label>
+        <InputText type='text' id='eventName'placeholder='Digite o nome do evento'
+         {...formik.getFieldProps('name')}/>
+         {formik.touched.name ? <ErrorInputs>{formik.errors.name}</ErrorInputs> : null}
 
-          <Label htmlFor='eventDate'>Data do evento:</Label>
-          <InputText type='date' id='eventDate'placeholder='Digite a data do evento'/>
+        <Label htmlFor='eventType'>Tipo de evento: </Label>
+        <InputText type='text' id='eventType'placeholder='Digite o tipo do evento'
+        {...formik.getFieldProps('type')}/>
+        {formik.touched.type ? <ErrorInputs>{formik.errors.type}</ErrorInputs> : null}
+        
 
-          <Label htmlFor='priceByTicket'>Preço por ingresso - R$</Label>
-          <InputText
-            type='number'
-            placeholder='Digite o valor do ingresso. Ex: 200'
-            id='priceByTicket'
-          />
+        <Label htmlFor='eventTicketNumber'>Quantidade de ingressos:</Label>
+        <InputText type='number' id='eventTicketNumber'placeholder='Digite a quantidade de ingressos'
+        {...formik.getFieldProps('quantity')}/>
+        {formik.touched.quantity ? <ErrorInputs>{formik.errors.quantity}</ErrorInputs> : null}
 
-          <Label htmlFor='description'>Descrição:</Label>
-          <InputTextarea id='description'placeholder='Digite a descrição do evento'/> 
-           
-            <div className={classes.div}>
+        <Label htmlFor='eventDate'>Data do evento:</Label>
+        <InputText type='date' id='eventDate'placeholder='Digite a data do evento'
+        {...formik.getFieldProps('date')}/>
+        {formik.touched.date ? <ErrorInputs>{formik.errors.date}</ErrorInputs> : null}
 
-            <Button
-                    variant="contained"
-                    color="primary"
-                    startIcon={<SaveIcon />}
-                    className={classes.button}>
-            Criar evento
-            </Button>
-            </div>
-      
-          </FormCardWrapper>
-        </Container>
-        <Footer/>
-      </>
-    );
-  }
-  
+        <Label htmlFor='priceByTicket'>Preço por ingresso - R$</Label>
+        <InputText
+          type='number'
+          placeholder='Digite o valor do ingresso. Ex: 200'
+          id='priceByTicket'
+        {...formik.getFieldProps('price')}/>
+        {formik.touched.price ? <ErrorInputs>{formik.errors.price}</ErrorInputs> : null}
+
+        <Label htmlFor='description'>Descrição:</Label>
+        <InputTextarea id='description'placeholder='Digite a descrição do evento'
+        {...formik.getFieldProps('description')}/>
+        {formik.touched.description ? <ErrorInputs>{formik.errors.description}</ErrorInputs> : null}
+         
+          <div className={classes.div}>
+
+          <Button
+          type="submit"
+                  variant="contained"
+                  color="primary"
+                  startIcon={<SaveIcon />}
+                  className={classes.button}>
+          Criar evento
+          </Button>
+          </div>
+    
+        </FormCardWrapper>
+      </Container>
+      <Footer/>
+    </>
+  );
+}
---
+++ frontend/src/pages/Event/style.js
@@ -0,0 +1,144 @@
+
+import styled from 'styled-components';
+
+export const PageTitle = styled.h2`
+background-color: black;
+color: white;
+width: 100%;
+height: 54px;
+font-size: 1.5rem;
+line-height: 54px;
+margin: 0;
+text-align: center;
+font-weight: 400;
+@media(min-width: 768px ){
+    height: 50px;
+    line-height: 50px;
+}
+
+`;
+export const FormCardWrapper = styled.form`
+padding: 1rem;
+@media(min-width: 768px ){
+    border-radius: 0.5rem;
+    margin-top: 3rem;
+    text-align: center;
+    box-shadow: 0px 0px 10px rgba(7, 7, 7, 0.589);
+    padding: 1rem;
+};
+
+input[type="date"]{
+    text-transform: uppercase;
+}
+`;
+export const FormTitle = styled.h3 `
+    font-size: 1.58rem;
+    margin-bottom: 1rem;
+`
+export const DivCardWrapper = styled.div`
+padding: 1rem;
+@media(min-width: 768px ){
+    border-radius: 0.5rem;
+    margin-top: 3rem;
+    text-align: center;
+    box-shadow: 0px 0px 10px rgba(7, 7, 7, 0.589);
+    padding: 1rem;
+    height: 800px;
+    
+};
+
+input[type="date"]{
+    text-transform: lowercase;
+}
+`;
+export const InputText = styled.input`
+height: 2.5rem;
+width: 100%;
+font-size: 1rem;
+padding-left: 0.5rem;
+border: 1px solid black ;
+border-radius: 0.2rem;
+@media(min-width: 768px ){
+    &:focus{
+        outline: none;
+        box-shadow: 0px 0px 5px 1px  rgb(139, 187, 243); 
+    }
+`;
+
+export const ErrorInputs = styled.div`
+    width: 100%;
+    text-align: left;
+    font-size: 14px;
+    margin-top: 5px;
+    color: red;
+`;
+
+export const InputTextarea = styled.textarea`
+resize:none;
+height: 2.5rem;
+width: 100%;
+font-size: 1rem;
+min-height: 150px;
+padding: 5px 8px;
+text-align: justify;
+border: 1px solid black ;
+border-radius: 0.2rem;
+outline: none;
+
+`;
+
+export const Label = styled.label`
+display: block ;
+text-align: left;
+margin-bottom: 0.4rem ;
+margin-top: 1rem;
+`
+
+export const ButtonSubmit = styled.button` 
+
+@media(min-width: 768px ){
+    width: 70%;
+    margin-left: auto;
+    margin-right: auto;
+}
+
+cursor: pointer;
+width: 100%;
+height: 45px;
+color: white;
+font-size: 1.2rem;
+border: none;
+border-radius: 5px;
+margin-top: 1rem;
+background-color: ${props => {
+    
+    const {buttonStyleType} = props
+    if(buttonStyleType === "delete"){
+        return "#B91818"
+    }
+    
+    if(buttonStyleType === "update"){
+        return "#0974ef"
+    }
+    if(buttonStyleType === "create"){
+        return "#18B91E"
+    }
+    console.log(buttonStyleType)
+}};
+` 
+// 
+export const Container = styled.div`
+    @media(min-width: 768px ){
+        max-width: 500px;
+        margin: 0 auto;
+        margin-bottom: 2rem;
+        padding: 0;
+
+    ${PageTitle}{
+        height: 50px;
+        line-height: 50px;
+        color: red;
+    }
+}
+
+`
