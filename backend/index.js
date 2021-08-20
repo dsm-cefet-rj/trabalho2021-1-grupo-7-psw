dee82773dbd3a1d6f037d7b6f9ee26ded4d37963
lucasfarolfi <lucasfarolfi.rj@gmail.com>
2021-08-20 14:03:05 -0300

    Instalação do cors

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
 const Router = require('./Router/router')
+const cors = require('cors')
 
 //Arquivo de start do servidor
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: false}))
 app.use(express.json())
+app.use(cors())
 
 app.use("/admin", Router)
-
 
 app.listen(8080, () => console.log("Server is running"))
