f2122fd3f1813f857803095c56c8ab8363af967c
dreymond1 <andrey.alves9@gmail.com>
2021-09-17 19:55:20 -0300

    Uso do mongo na criação e confirmação de eventos

---
+++ backend/models/events.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+
+const eventSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  slug: {
+    type: String,
+    require: true,
+  },
+  type: {
+    type: String,
+    require: true,
+  },
+  description: {
+    type: String,
+    require: true,
+  },
+  company: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "company",
+    require: true,
+  },
+  num_tickets: {
+    type: Number,
+    require: true,
+  },
+  date: {
+    type: String,
+    require: true,
+  },
+  price: {
+    type: Number,
+    require: true,
+  },
+  address: {
+    long: { type: Number, require: true },
+    lat: { type: Number, require: true },
+  },
+  images: {
+    type: String,
+    require: true,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+let Event = mongoose.model("event", eventSchema);
+
+module.exports = Event;
