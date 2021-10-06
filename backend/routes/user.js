2b883c0430088f96ed97973b85c84ae6247e08b0
João Marcos <joaomarcossleite@gmail.com>
2021-10-06 18:41:37 -0300

    add: authentication to front

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -10,7 +10,7 @@ const getToken = require('../middlewares/authenticate');
 const passport = require('passport');
 
 //Pega todos os usuários
-router.get('/',auth.verifyUser, auth.userAdmin, async (req, res, next) => {
+router.get('/', auth.verifyUser, auth.userAdmin, async (req, res, next) => {
   try {
     let users = await User.find({});
     res.status(200).json(users);
@@ -20,7 +20,7 @@ router.get('/',auth.verifyUser, auth.userAdmin, async (req, res, next) => {
 });
 
 //Pegar um usuário
-router.get('/:id',auth.verifyUser,auth.userAdmin, async (req, res, next) => {
+router.get('/:id', auth.verifyUser, auth.userAdmin, async (req, res, next) => {
   let id = req.params.id;
 
   if (!verifyUser.id(id)) {
@@ -40,15 +40,14 @@ router.get('/:id',auth.verifyUser,auth.userAdmin, async (req, res, next) => {
 router.post('/', async (req, res, next) => {
   try {
     let { name, email, document, password } = req.body;
-
     let verifyData = verifyUser.user(name, email, document, password);
     if (!verifyData) {
       return res.status(400).json({ msg: 'Dados inválidos.' });
     }
 
-    let user_role
-    if(document.length == 14) user_role = 0
-    else user_role = 1
+    let user_role;
+    if (document.length == 14) user_role = 0;
+    else user_role = 1;
 
     let userFound = await User.findOne({ email: email });
     let companyFound = await Company.findOne({ email: email });
@@ -57,58 +56,82 @@ router.post('/', async (req, res, next) => {
       return res.status(401).json({ msg: 'E-mail já cadastrado.' });
     }
 
-    User.register(new User({name,email, document, role:user_role, username: email}), password, (err, user) =>{
-      if(err){
-        res.status(500).json({ msg: 'Erro ao criar usuário' });
-      }else{
-        return res.status(200).json({ msg: 'Usuário cadastrado com sucesso' });
-        /*
+    User.register(
+      new User({ name, email, document, role: user_role, username: email }),
+      password,
+      (err, user) => {
+        if (err) {
+          res.status(500).json({ msg: 'Erro ao criar usuário' });
+        } else {
+          return res
+            .status(200)
+            .json({ msg: 'Usuário cadastrado com sucesso' });
+          /*
         passport.authenticate('local')(req,res, ()=>{
           console.log("chegou aqui")
           res.statusCode = 200
           res.setHeader("Content-Type", "application/json")
           res.json({ success: true, status: "Usuário cadastrado com sucesso!"})
         })*/
+        }
       }
-    })
-
+    );
   } catch (e) {
     res.status(500).json({ msg: 'Erro interno' });
   }
 });
 
 //Login do usuário
-router.post("/login", passport.authenticate("local"),async (req, res, next) => {
-  let token = auth.getToken({ _id: req.user._id});
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "application/json");
-  res.json({success: true, token: token, status: "Login feito com sucesso!",
-  user:{name: req.user.name, email: req.user.email, role: req.user.role, id: req.user._id, document: req.user.document}});
-});
+router.post(
+  '/login',
+  passport.authenticate('local'),
+  async (req, res, next) => {
+    let token = auth.getToken({ _id: req.user._id });
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({
+      success: true,
+      token: token,
+      status: 'Login feito com sucesso!',
+      user: {
+        name: req.user.name,
+        email: req.user.email,
+        role: req.user.role,
+        id: req.user._id,
+        document: req.user.document,
+      },
+    });
+  }
+);
 
 //Deleta um usuário
-router.delete('/:id',auth.verifyUser,auth.userAdmin, async (req, res, next) => {
-  try {
-    let id = req.params.id;
+router.delete(
+  '/:id',
+  auth.verifyUser,
+  auth.userAdmin,
+  async (req, res, next) => {
+    try {
+      let id = req.params.id;
 
-    if (!verifyUser.id(id)) {
-      return res.status(400).json({ msg: 'Dados inválidos.' });
+      if (!verifyUser.id(id)) {
+        return res.status(400).json({ msg: 'Dados inválidos.' });
+      }
+
+      let userExists = await User.findById(id).exec();
+      if (userExists == undefined) {
+        return res.status(404).json({ msg: 'Usuário não encontrado.' });
+      }
+
+      await User.findByIdAndDelete(id);
+      return res.status(200).json({ status: 'Usuário deletado com sucesso.' });
+    } catch (e) {
+      res.status(500).json({ msg: 'Erro interno' });
     }
-
-    let userExists = await User.findById(id).exec();
-    if (userExists == undefined) {
-      return res.status(404).json({ msg: 'Usuário não encontrado.' });
-    }
-
-    await User.findByIdAndDelete(id);
-    return res.status(200).json({ status: 'Usuário deletado com sucesso.' });
-  } catch (e) {
-    res.status(500).json({ msg: 'Erro interno' });
   }
-});
+);
 
 //Atualiza um usuário
-router.put('/:id',auth.verifyUser,auth.userAdmin, async (req, res, next) => {
+router.put('/:id', auth.verifyUser, auth.userAdmin, async (req, res, next) => {
   try {
     let { name, email, cpf, password } = req.body;
     let id = req.params.id;
@@ -139,89 +162,108 @@ router.put('/:id',auth.verifyUser,auth.userAdmin, async (req, res, next) => {
   }
 });
 
-
 //Pega todos os favoritos do banco
-router.get('/favoritos/favoritos',auth.verifyUser,auth.userAdmin, async (req, res) => {
-  let favorites = await Favorite.find({});
-  if (favorites.length === 0) {
-    return res
-      .status(200)
-      .json({ msg: 'Nenhum favorito registrado.' })
-      .populate(['user', 'event']);
+router.get(
+  '/favoritos/favoritos',
+  auth.verifyUser,
+  auth.userAdmin,
+  async (req, res) => {
+    let favorites = await Favorite.find({});
+    if (favorites.length === 0) {
+      return res
+        .status(200)
+        .json({ msg: 'Nenhum favorito registrado.' })
+        .populate(['user', 'event']);
+    }
+    res.json(favorites);
   }
-  res.json(favorites);
-});
+);
 
 //Pega os favoritos do usuário
-router.get('/:id/favoritos',auth.verifyUser,auth.userClient, async (req, res, next) => {
-  let id = req.params.id;
+router.get(
+  '/:id/favoritos',
+  auth.verifyUser,
+  auth.userClient,
+  async (req, res, next) => {
+    let id = req.params.id;
 
-  if (!verifyDataUser.id(id)) {
-    return res.status(400).json({ msg: 'Dados inválidos.' });
-  }
+    if (!verifyDataUser.id(id)) {
+      return res.status(400).json({ msg: 'Dados inválidos.' });
+    }
 
-  let userFound = await User.findOne({ _id: id });
-  if (userFound == undefined) {
-    return res.status(404).json({ msg: 'Usuário não encontrado.' });
-  }
+    let userFound = await User.findOne({ _id: id });
+    if (userFound == undefined) {
+      return res.status(404).json({ msg: 'Usuário não encontrado.' });
+    }
 
-  let getFavorites = await Favorite.find({ user: id }).populate([
-    'user',
-    'event',
-  ]);
-  if (getFavorites.length === 0) {
-    return res.status(200).json({ msg: 'Nenhum favorito registrado.' });
+    let getFavorites = await Favorite.find({ user: id }).populate([
+      'user',
+      'event',
+    ]);
+    if (getFavorites.length === 0) {
+      return res.status(200).json({ msg: 'Nenhum favorito registrado.' });
+    }
+    res.status(200).json(getFavorites);
   }
-  res.status(200).json(getFavorites);
-});
+);
 
 //Adiciona favorito
-router.post('/:user_id/favoritos/:event_id',auth.verifyUser, auth.userClient,async (req, res, next) => {
-  let { user_id, event_id } = req.params;
+router.post(
+  '/:user_id/favoritos/:event_id',
+  auth.verifyUser,
+  auth.userClient,
+  async (req, res, next) => {
+    let { user_id, event_id } = req.params;
 
-  if (!verifyDataUser.id(user_id) || !verifyDataUser.id(event_id)) {
-    return res.status(400).json({ msg: 'Dados inválidos.' });
+    if (!verifyDataUser.id(user_id) || !verifyDataUser.id(event_id)) {
+      return res.status(400).json({ msg: 'Dados inválidos.' });
+    }
+
+    let userFound = await User.findOne({ _id: user_id });
+    if (userFound == undefined) {
+      return res.status(404).json({ msg: 'Usuário não encontrado.' });
+    }
+
+    let eventFound = await Event.findOne({ _id: event_id });
+    if (eventFound == undefined) {
+      return res.status(404).json({ msg: 'Evento não encontrado.' });
+    }
+
+    let favorite = new Favorite({
+      user: user_id,
+      event: event_id,
+    });
+
+    favorite.save();
+    res.status(200).json({ status: 'Favorito adicionado com sucesso.' });
   }
-
-  let userFound = await User.findOne({ _id: user_id });
-  if (userFound == undefined) {
-    return res.status(404).json({ msg: 'Usuário não encontrado.' });
-  }
-
-  let eventFound = await Event.findOne({ _id: event_id });
-  if (eventFound == undefined) {
-    return res.status(404).json({ msg: 'Evento não encontrado.' });
-  }
-
-  let favorite = new Favorite({
-    user: user_id,
-    event: event_id,
-  });
-
-  favorite.save();
-  res.status(200).json({ status: 'Favorito adicionado com sucesso.' });
-});
+);
 
 //Deleta favorito
-router.delete('/:user_id/favoritos/:favorite_id',auth.verifyUser, auth.userClient, async (req, res, next) => {
-  let { user_id, favorite_id } = req.params;
+router.delete(
+  '/:user_id/favoritos/:favorite_id',
+  auth.verifyUser,
+  auth.userClient,
+  async (req, res, next) => {
+    let { user_id, favorite_id } = req.params;
 
-  if (!verifyDataUser.id(user_id) || !verifyDataUser.id(favorite_id)) {
-    return res.status(400).json({ msg: 'Dados inválidos.' });
+    if (!verifyDataUser.id(user_id) || !verifyDataUser.id(favorite_id)) {
+      return res.status(400).json({ msg: 'Dados inválidos.' });
+    }
+
+    let userFound = await User.findOne({ _id: user_id });
+    if (userFound == undefined) {
+      return res.status(404).json({ msg: 'Usuário não encontrado.' });
+    }
+
+    let favoriteFound = await Favorite.findOne({ _id: favorite_id });
+    if (favoriteFound == undefined) {
+      return res.status(404).json({ msg: 'Favorito não encontrado.' });
+    }
+
+    await Favorite.findByIdAndDelete(favorite_id);
+    return res.status(200).json({ status: 'Favorito deletado com sucesso.' });
   }
-
-  let userFound = await User.findOne({ _id: user_id });
-  if (userFound == undefined) {
-    return res.status(404).json({ msg: 'Usuário não encontrado.' });
-  }
-
-  let favoriteFound = await Favorite.findOne({ _id: favorite_id });
-  if (favoriteFound == undefined) {
-    return res.status(404).json({ msg: 'Favorito não encontrado.' });
-  }
-
-  await Favorite.findByIdAndDelete(favorite_id);
-  return res.status(200).json({ status: 'Favorito deletado com sucesso.' });
-});
+);
 
 module.exports = router;
--- frontend/src/routes/Routes.js
+++ frontend/src/routes/Routes.js
@@ -1,39 +1,66 @@
-import React from 'react'
-import { Router, Switch, Route } from 'react-router-dom'
-import { history } from '../history'
+import React from 'react';
+import { Router, Switch, Route, Redirect } from 'react-router-dom';
+import { history } from '../history';
 
 //Componentes das páginas
-import Home from '../pages/home/home'
-import EventPage from '../pages/EventPage/index.jsx'
-import MyTickets from '../pages/myTickets/myTickets'
-import Login from '../pages/Login/login'
-import RegisteredEvents from '../pages/registeredEvents/registeredEvents'
-import RegisterC from '../pages/RegisterComp'
-import RegisterU from '../pages/RegisterUser'
-import Page404 from '../pages/Page404'
-import EventReadAndDelete from '../pages/Event/ReadAndDelete'
-import EventUpdate from '../pages/Event/Update'
-import EventCreate from '../pages/Event/Create'
-import AboutUs from '../pages/About/about'
-import Feedback from '../pages/Feedback/index'
+import Home from '../pages/home/home';
+import EventPage from '../pages/EventPage/index.jsx';
+import MyTickets from '../pages/myTickets/myTickets';
+import Login from '../pages/Login/login';
+import RegisteredEvents from '../pages/registeredEvents/registeredEvents';
+import RegisterC from '../pages/RegisterComp';
+import RegisterU from '../pages/RegisterUser';
+import Page404 from '../pages/Page404';
+import EventReadAndDelete from '../pages/Event/ReadAndDelete';
+import EventUpdate from '../pages/Event/Update';
+import EventCreate from '../pages/Event/Create';
+import AboutUs from '../pages/About/about';
+import Feedback from '../pages/Feedback/index';
+
+const isAuthentticated = () => {
+  if (!localStorage.getItem('user')) {
+    return false;
+  } else {
+    return true;
+  }
+};
 
 export default function Routes() {
   return (
-    <Router history={history}> {/* Dentro deste componente há uma função q o Router espera receber */}
-      <Switch> {/* Onde contém todas as rotas da aplicação */}
-          <Route exact path="/" component={Home}/>
-          <Route exact path="/sobre" component={AboutUs}/>
-          <Route exact path="/evento/:slug" component={EventPage}/>
-          <Route exact path="/entrar" component={Login}/>
-          <Route exact path="/cadastrar" component={RegisterU}/>
-          <Route exact path="/admin/cadastrar" component={RegisterC}/>
-          <Route exact path="/meus-ingressos" component={MyTickets}/>
-          <Route exact path="/admin/eventos" component={RegisteredEvents}/>
-          <Route exact path="/feedback"  component={Feedback}/>
-          <Route exact path="/admin/evento/novo"  component={EventCreate}/>
-          <Route exact path="/admin/evento/:slug/editar"  component={EventUpdate}/>
-          <Route exact path="/admin/evento/:slug"  component={EventReadAndDelete}/>
-          <Route path="*" component={Page404}/>{/*Se a rota não existir... */}
+    <Router history={history}>
+      {!isAuthentticated() && (
+        <Redirect
+          to={{
+            pathname: '/entrar',
+          }}
+        />
+      )}
+      {/* Dentro deste componente há uma função q o Router espera receber */}
+      <Switch>
+        {' '}
+        {/* Onde contém todas as rotas da aplicação */}
+        <Route exact path='/' component={Home} />
+        <Route exact path='/sobre' component={AboutUs} />
+        <Route exact path='/evento/:slug' component={EventPage} />
+        <Route exact path='/entrar' component={Login} />
+        <Route exact path='/cadastrar' component={RegisterU} />
+        <Route exact path='/admin/cadastrar' component={RegisterC} />
+        <Route exact path='/meus-ingressos' component={MyTickets} />
+        <Route exact path='/admin/eventos' component={RegisteredEvents} />
+        <Route exact path='/feedback' component={Feedback} />
+        <Route exact path='/admin/evento/novo' component={EventCreate} />
+        <Route
+          exact
+          path='/admin/evento/:slug/editar'
+          component={EventUpdate}
+        />
+        <Route
+          exact
+          path='/admin/evento/:slug'
+          component={EventReadAndDelete}
+        />
+        <Route path='*' component={Page404} />
+        {/*Se a rota não existir... */}
       </Switch>
     </Router>
   );
